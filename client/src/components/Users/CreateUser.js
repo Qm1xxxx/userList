39e76ea7a419a07cd5be75a2354d79d195935bde
Qmix <qmix121212@gmail.com>
2025-06-02 19:59:51 +0300

    Реализовали модальное окно ErrorModal

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -1,60 +1,83 @@
 import { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
-import styles from './CreateUser.module.css';
+import styles from './CreateUser.module.css'
+import ErrorModal from '../UI/ErrorModal';
 
-const CreateUser = () => {
-  const [inputName, setInputName] = useState('');
-  const [inputAge, setInputAge] = useState('');
+const CreateUser = (props) => {
 
-  const createUserHandler = (event) => {
-    event.preventDefault();
-    console.log('Имя:', inputName);
-    console.log('Возраст:', inputAge);
+    const [inputName, setInputName] = useState('');
+    const [inputAge, setInputAge] = useState('');
+    const [error, setError] = useState()
 
-    if(inputName.trim().length === 0){
-        return
+    const createUserHandler = (event) => {
+        event.preventDefault()
+
+
+        if (inputName.trim().length === 0) {
+
+            setError({
+                title: 'Неккоректный ввод',
+                message: 'Поля для ввода не могут быть пустыми!'
+            })
+
+            return
+        }
+
+        if (+inputAge < 1 || +inputAge > 100) {
+
+            setError({
+                title: 'Неккоректный ввод',
+                message: 'Возраст должен быть выше ноля!'
+            })
+
+            return
+        }
+
+        props.onCreateUser(inputName, inputAge)
+
+        setInputName('');
+        setInputAge('');
     }
-    if(+inputAge < 1){
-        return
+
+    const nameChangeHandler = (event) => {
+        setInputName(event.target.value)
     }
-   
-  };
 
-  props.onCreateUser(inputName, inputAge)
+    const ageChangeHandler = (event) => {
+        setInputAge(event.target.value)
+    }
 
-  const nameChangeHandler = (event) => {
-    setInputName(event.target.value);
-  };
+    return (
+        <>
 
-  const ageChangeHandler = (event) => {
-    setInputAge(event.target.value);
-  };
+            {error && <ErrorModal
+                title={error.title}
+                message={error.message}>
+            </ErrorModal>}
+            <Card className={styles.input}>
+                <form onSubmit={createUserHandler}>
+                    <label htmlFor="name">Имя</label>
+                    <input id='name'
+                        type='text'
+                        onChange={nameChangeHandler}
+                        value={inputName} />
 
-  return (
-    <Card className={styles.input}>
-      <form onSubmit={createUserHandler}>
-        <label htmlFor="name">Имя</label>
-        <input
-          id="name"
-          type="text"
-          value={inputName}
-          onChange={nameChangeHandler}
-        />
+                    <label htmlFor="age">Возраст</label>
+                    <input id='age'
+                        type='number'
+                        onChange={ageChangeHandler}
+                        value={inputAge} />
 
-        <label htmlFor="age">Возраст</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={inputAge} />
-        
+                    {/* <button type='submit'>Добавить пользователя</button> */}
+                    <Button type="submit">Добавить пользователя</Button>
 
-        
-        <Button type="submit">Добавить пользователя</Button>
-      </form>
-    </Card>
-  );
-};
+                </form>
+            </Card>
+            
+        </>
+    )
+
+}
 
 export default CreateUser;
